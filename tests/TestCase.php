7bdd51169b1d06a5464cbcf58bc61a8f4778f9ed
Ari Miller <arimiller92@gmail.com>
2017-04-27 21:48:09 -0400

    add exception handling for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,10 @@
 
 namespace Tests;
 
+use Tests\Utils\HandlesExceptions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, HandlesExceptions;
 }
---
+++ tests/Utils/HandlesExceptions.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Utils;
+
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+
+/**
+ * As of Laravel 5.4.19, exceptions thrown by the application
+ * are usually caught in order to give the user a simple 500.
+ * However, for testing this is not ideal as we cannot easily
+ * see what the error is. Adam Wathan authored this solution.
+ */
+trait HandlesExceptions
+{
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->disableExceptionHandling();
+    }
+
+    /**
+     * Disable the default Laravel exception handling.
+     *
+     * @return void
+     */
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            /**
+             * Implementing this constructor is required by the interface.
+             */
+            public function __construct()
+            {
+                //
+            }
+
+            /**
+             * Implementing report is required here.
+             */
+            public function report(\Exception $e)
+            {
+                //
+            }
+
+            /**
+             * Here is where we actually re throw the exception so
+             * that our test will be able to receive the error.
+             *
+             * @param  $request
+             * @param  \Exception $e
+             * @return void
+             */
+            public function render($request, \Exception $e)
+            {
+                throw $e;
+            }
+        });
+    }
+
+    /**
+     * Turn back on the default Laravel method of catching exceptions.
+     *
+     * @return $this
+     */
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
+
+        return $this;
+    }
+}

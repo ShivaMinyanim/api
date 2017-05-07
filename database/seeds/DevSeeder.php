ec0ea63367c282832f7fd0545d11e0403456412c
Ari Miller <arimiller92@gmail.com>
2017-05-07 13:14:50 -0400

    add dev seeder

--- database/seeds/DevSeeder.php
+++ database/seeds/DevSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+use App\Models\House;
 use App\Models\Minyan;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,32 @@ class DevSeeder extends Seeder
      */
     public function run()
     {
-        // create(Minyan::class);
+        $this->create_house_with_minyanim_for_days($days = 2);
+        $this->create_house_with_minyanim_for_days($days = 2);
+    }
+
+    /**
+     * Create a shiva house with minyanim for each day of the
+     * given day count.
+     *
+     * @param  int    $days
+     * @return [type]
+     */
+    protected function create_house_with_minyanim_for_days(int $days)
+    {
+        $house = create(House::class);
+
+        for ($day = 0; $day < $days; $day++) {
+            create(Minyan::class, [
+                'type' => 'shacharis',
+                'house_id' => $house->id,
+                'timestamp' => Carbon::today()->addHours(7)->addDays($day)
+            ]);
+            create(Minyan::class, [
+                'type' => 'mincha/maariv',
+                'house_id' => $house->id,
+                'timestamp' => Carbon::today()->addHours(18)->addDays($day)
+            ]);
+        }
     }
 }
